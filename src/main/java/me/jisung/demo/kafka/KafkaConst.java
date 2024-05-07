b842f641fb78f6c454ea977c5649c3d6b4808090
zzzzseong <jung48182@gmail.com>
2024-05-07 14:19:54 +0900

    consumer group property const로 이동

--- src/main/java/me/jisung/demo/kafka/KafkaConst.java
+++ src/main/java/me/jisung/demo/kafka/KafkaConst.java
@@ -4,6 +4,9 @@ package me.jisung.demo.kafka;
  * interface for store kafka topic literal
  * */
 public interface KafkaConst {
+
+    String CONSUMER_GROUP = "demo-group";
+
     String KAFKA_TOPIC_DEMO = "demo";
     String KAFKA_TOPIC_DEMO_MANUAL = "demo_manual";
 }
