47f042cf8a5367381525c0b2aeffee21f8a1589e
zzzzseong <jung48182@gmail.com>
2024-05-10 13:35:27 +0900

    consumer code refactor

--- src/main/java/me/jisung/demo/kafka/KafkaConsumer.java
+++ src/main/java/me/jisung/demo/kafka/KafkaConsumer.java
@@ -21,15 +21,7 @@ public class KafkaConsumer {
      * */
     @KafkaListener(topics = KafkaConst.KAFKA_TOPIC_DEMO)
     public void consumeDemo(ConsumerRecords<String, String> records) {
-
-        // Schedulers.boundsElastic()
-        // Reactor에서 제공하는 Scheduler로 비동기 작업을 처리하고, boundedElastic으로 동적으로 크기를 조절하는 스레드 풀 사용
-
-        Flux.fromIterable(records)
-                .subscribeOn(Schedulers.boundedElastic())
-                .doOnNext(record -> log.info("{}", record))
-                .doOnError((e) -> log.error(e.getMessage(), e))
-                .subscribe();
+        handleRecord(records);
     }
 
     /**
@@ -39,18 +31,20 @@ public class KafkaConsumer {
      * */
     @KafkaListener(topics = KafkaConst.KAFKA_TOPIC_DEMO_MANUAL)
     public void consumeDemoManual(ConsumerRecords<String, String> records, Acknowledgment ack) {
-
-        Flux.fromIterable(records)
-                .subscribeOn(Schedulers.boundedElastic())
-                .doOnNext(record -> log.info("{}", record))
-                .doOnError((e) -> log.error(e.getMessage(), e))
-                .subscribe();
-
+        handleRecord(records);
         ack.acknowledge();
     }
 
     @KafkaListener(topics = KafkaConst.KAFKA_TOPIC_STREAMS_FILTER)
     public void streams(ConsumerRecords<String, String> records) {
+        handleRecord(records);
+    }
+
+
+    private void handleRecord(ConsumerRecords<String, String> records) {
+        // Schedulers.boundsElastic()
+        // Reactor에서 제공하는 Scheduler로 비동기 작업을 처리하고, boundedElastic으로 동적으로 크기를 조절하는 스레드 풀 사용
+
         Flux.fromIterable(records)
                 .subscribeOn(Schedulers.boundedElastic())
                 .doOnNext(record -> log.info("{}", record))
